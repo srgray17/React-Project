1418e285e5ec11963c76513343b445b23b2697c5
srgray17 <srgray@lakeheadu.ca>
2021-02-24 14:15:14 -0500

    fixed home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,10 +8,9 @@ const Home = () => {
     <div className="container mt-3">
        <h3>Welcome </h3>
        <p>This application is a for an assignmment using React components.</p>
-       <p>For more information about this application please viste the About page.</p>
+       <p>For more information about this application please visit the About page.</p>
     </div>
     </>
-   
   );
 }
  
